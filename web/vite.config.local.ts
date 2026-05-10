fd525770b60ee597a10ded6d1ac635aff9d8fcfd
hujing <hujingnb@qq.com>
2026-05-10 15:22:30 +0800

    commit

---
+++ web/vite.config.local.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
+})
